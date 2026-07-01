58ff9ac38a22d2d506ac8364488c76edd7fc1853
Gatien Roche <gatien.roche0011@gmail.com>
2026-07-01 10:41:17 +0200

    Polices réelles : Intro.otf + GlacialIndifference, SW v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caddie-v5';
+const CACHE = 'caddie-v6';
 const ASSETS = [
   '/mes-courses/',
   '/mes-courses/index.html',
@@ -7,6 +7,9 @@ const ASSETS = [
   '/mes-courses/logo-icon-v2.jpg',
   '/mes-courses/logo-header.jpg',
   '/mes-courses/logo-login.jpg',
+  '/mes-courses/Intro.otf',
+  '/mes-courses/GlacialIndifference-Regular.otf',
+  '/mes-courses/GlacialIndifference-Bold.otf',
 ];
 
 self.addEventListener('install', e => {
